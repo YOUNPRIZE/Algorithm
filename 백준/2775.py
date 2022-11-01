256ebf27ad41cd9895d6495c3dfa89e2993a0f30
YOUNSANG CHO <ysang10@gmail.com>
2022-11-01 11:53:25 +0900

    solved

---
+++ 백준/2775.py
@@ -0,0 +1,16 @@
+# 백준 - DP
+## 브론즈 5
+### 부녀회장이 될테야
+
+t = int(input())
+
+for _ in range(t):
+    floor = int(input())
+    num = int(input())
+    
+    people = [i for i in range(1, num+1)]
+
+    for x in range(floor):
+        for y in range(1, num):
+            people[y] += people[y-1]
+    print(people[-1])
