44601ad6dc47f5b6c55bc86eb4215f20a0617b37
YOUNSANG CHO <ysang10@gmail.com>
2022-11-17 23:13:16 +0900

    solved

---
+++ 백준/14501.py
@@ -0,0 +1,25 @@
+# 백준 - Brute Force
+## 실버 3
+### 퇴사
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+t_list = []
+p_list = []
+answer = [0] * (n + 1)
+
+for i in range(n):
+    t, p = map(int, input().split())
+    t_list.append(t)
+    p_list.append(p)
+
+for i in range(n - 1, -1, -1):
+    if t_list[i] + i > n:
+        answer[i] = answer[i + 1]
+    else:
+        answer[i] = max(p_list[i] + answer[i + t_list[i]], answer[i + 1])
+        
+print(answer[0])
